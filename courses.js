3ac709e843c4b0588614f93f514898468567af95
Matthew Cocco <hello@matthewcocco.com>
2017-04-25 18:54:14 -0500

    planmaker proto commit for presentation

---
+++ courses.js
@@ -0,0 +1,141 @@
+var courses = [
+	{
+		code:"RHET 1302",
+		title:"Rhetoric"
+	},{
+		code:"COMM 1311",
+		title:"Communications"
+	},{
+		code:"HIST 1301",
+		title:"American History 1"
+	},{
+		code:"HIST 1302",
+		title:"American History 2"
+	},{
+		code:"GOVT 2305",
+		title:"Government/Political Science 1"
+	},{
+		code:"GOVT 2306",
+		title:"Government/Political Science 2"
+	},{
+		code:"AMS 2300",
+		title:"Language/Philosophy/Culture"
+	},{
+		code:"HUMA 1301",
+		title:"Creative Arts"
+	},{
+		code:"CHEM 1311",
+		title:"Life & Physical Sciences"
+	},{
+		code:"MATH 1325",
+		title:"Applied Calculus 1"
+	},{
+		code:"MATH 1326",
+		title:"Applied Calculus 2"
+	},{
+		code:"ECON 2301",
+		title:"Macroeconomics"
+	},{
+		code:"ECON 2302",
+		title:"Microeconomics"
+	},{
+		code:"ACCT 2301",
+		title:"Intro Financial Accounting"
+	},{
+		code:"ACCT 2302",
+		title:"Intro Managerial Accounting"
+	},{
+		code:"ITSS 3100",
+		title:"Professional Development"
+	},{
+		code:"BLAW 2301",
+		title:"Business and Public Law"
+	},{
+		code:"OPRE 3360",
+		title:"Management Methods/Stats"
+	},{
+		code:"OPRE 3333",
+		title:"Matrices/Vectors"
+	},{
+		code:"BCOM 3310",
+		title:"Business Communication"
+	},{
+		code:"OBHR 3310",
+		title:"Organizational Behavior"
+	},{
+		code:"IMS 3310",
+		title:"International Business"
+	},{
+		code:"ITSS 3300",
+		title:"Intro to Mgmt Info Systems"
+	},{
+		code:"MKT 3300",
+		title:"Principles of Marketing"
+	},{
+		code:"BCOM 4350",
+		title:"Advanced Business Communication"
+	},{
+		code:"FIN 3320",
+		title:"Business Finance"
+	},{
+		code:"OPRE 3310",
+		title:"Operations Management"
+	},{
+		code:"BPS 4305",
+		title:"Strategic Management"
+	},{
+		code:"ITSS 3211",
+		title:"Introduction to Programming"
+	},{
+		code:"ITSS 3312",
+		title:"Object-Oriented Programming"
+	},{
+		code:"ITSS 4300",
+		title:"Database Fundamentals"
+	},{
+		code:"ITSS 4330",
+		title:"Systems Analysis and Design"
+	},{
+		code:"ITSS 4360",
+		title:"Network and Information Security"
+	},{
+		code:"ITSS 4351",
+		title:"Foundations of Business Intelligence"
+	},{
+		code:"ITSS 4370",
+		title:"Information Technology Management"
+	},{
+		code:"ITSS 4390",
+		title:"Information Systems Capstone"
+	},{
+		code:"ITSS 4v95",
+		title:"Seminar Series in Info Systems: Python"
+	},{
+		code:"ITSS 4v90",
+		title:"Internship in ITS"
+	},{
+		code:"ITSS 4352",
+		title:"Introduction to Web Analytics"
+	},{
+		code:"ITSS 4353",
+		title:"Business Analytics"
+	},{
+		code:"ITSS 4354",
+		title:"Managing Big Data"
+	},{
+		code:"ITSS 4355",
+		title:"Data Visualization"
+	},{
+		code:"ITSS 4340",
+		title:"Enterprise Resource Planning"
+	},{
+		code:"ITSS 4343",
+		title:"Integrated SCM Information Systems"
+	},{
+		code:"ITSS 4342",
+		title:"Analysis and Design of Accounting Systems"
+	},{
+		code:"ITSS 4380",
+		title:"Advanced Database Management"
+	}
+]
